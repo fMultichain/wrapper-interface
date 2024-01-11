c77ea5181d18f9eb59e19a03eab473d32e7b7802
Buns Enchantress <68980965+BunsDev@users.noreply.github.com>
2024-01-11 01:31:35 -0600

    update: verbiage

--- packages/nextjs/components/wrapper-ui/ContractInteraction.tsx
+++ packages/nextjs/components/wrapper-ui/ContractInteraction.tsx
@@ -25,7 +25,7 @@ export const ContractInteraction = () => {
       <HareIcon className="absolute right-0 bottom-24" />
       <div className="flex flex-col w-full mx-5 sm:mx-8 2xl:mx-20">
         <div className="flex flex-col mt-6 px-7 py-8 bg-base-200 opacity-80 rounded-2xl shadow-lg border-2 border-primary">
-          <span className="text-4xl sm:text-6xl text-black">Upgrade to lz-FMULTI</span>
+          <span className="text-4xl sm:text-6xl text-black">Upgrade to lz-fMULTI</span>
           <div className="mt-8 flex flex-col sm:flex-row items-start sm:items-center gap-2 sm:gap-5">
             <input
               type="text"
