af89eff0be73e0a85da6d6d03ab4b31f3f7eb20a
Buns Enchantress <68980965+BunsDev@users.noreply.github.com>
2024-01-15 20:11:21 -0600

    update: links (WIP)

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -1,21 +1,21 @@
-// import { hardhat } from "wagmi/chains";
+// import { hardhat } from "wagmi/chains"
+import Link from "next/link";
 import { CurrencyDollarIcon } from "@heroicons/react/24/outline";
-// import { HeartIcon } from "@heroicons/react/24/outline";
-// import { SwitchTheme } from "~~/components/SwitchTheme";
+// import { HeartIcon } from "@heroicons/react/24/outline"
+// import { SwitchTheme } from "~~/components/SwitchTheme"
 import { useGlobalState } from "~~/services/store/store";
 
-// import { getTargetNetwork } from "~~/utils/scaffold-eth";
+// import { getTargetNetwork } from "~~/utils/scaffold-eth"
 
 /**
  * Site footer
  */
 export const Footer = () => {
   const nativeCurrencyPrice = useGlobalState(state => state.nativeCurrencyPrice);
-
   return (
     <div className="min-h-0 py-5 px-1 mb-11 lg:mb-0">
       <div>
-        <div className="fixed flex justify-between items-center w-full z-10 p-4 bottom-0 left-0 pointer-events-none">
+        <div className="fixed flex justify-between items-center w-full z-10 p-4 bottom-0 left-0">
           <div className="flex space-x-2 pointer-events-auto">
             {nativeCurrencyPrice > 0 && (
               <div className="btn btn-primary btn-sm font-normal cursor-auto gap-0">
@@ -31,10 +31,34 @@ export const Footer = () => {
             //   justifyContent: 'center',
             // }}
           >
-            <div className={"flex justify-center p-2 border rounded-2xl"}>{`Docs`}</div>
-            <div className={"flex justify-center p-2 border rounded-2xl"}>{`Twitter`}</div>
-            <div className={"flex justify-center p-2 border rounded-2xl"}>{`Discord`}</div>
-            <div className={"flex justify-center p-2 border rounded-2xl"}>{`Telegram`}</div>
+            <div className={"flex justify-center p-2 border rounded-2xl"}>
+              <Link
+                href="https://t.me/FuckMulti" // todo
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {`Docs`}
+              </Link>
+            </div>
+            <div className={"flex justify-center p-2 border rounded-2xl"}>
+              <Link href="https://x.com/FMultichain" target="_blank" rel="noopener noreferrer">
+                {`Twitter`}
+              </Link>
+            </div>
+            <div className={"flex justify-center p-2 border rounded-2xl"}>
+              <Link
+                href="https://discord.gg" // todo
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {`Discord`}
+              </Link>
+            </div>
+            <div className={"flex justify-center p-2 border rounded-2xl"}>
+              <Link href="https://t.me/FuckMulti" target="_blank" rel="noopener noreferrer">
+                {`Telegram`}
+              </Link>
+            </div>
           </div>
           {/* <SwitchTheme className="pointer-events-auto" /> */}
         </div>
