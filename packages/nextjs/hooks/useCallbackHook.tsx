1d3be67d18dd79d113620b7b008e443d44958530
Buns Enchantress <68980965+BunsDev@users.noreply.github.com>
2024-01-23 23:15:38 -0600

    + Hooks: useCallbackHook

---
+++ packages/nextjs/hooks/useCallbackHook.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useRef } from "react";
+import { useIsomorphicLayoutEffect } from "usehooks-ts";
+
+export function useTimeoutHook(callback: () => void, delay: number | null) {
+  const savedCallback = useRef(callback);
+
+  // Remember the latest callback if it changes.
+  useIsomorphicLayoutEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // sets: timeout.
+  useEffect(() => {
+    // Don't schedule if no delay is specified.
+    // Note: 0 is a valid value for delay.
+    if (!delay && delay !== 0) {
+      return;
+    }
+
+    const id = setTimeout(() => savedCallback.current(), delay);
+
+    return () => clearTimeout(id);
+  }, [delay]);
+}
