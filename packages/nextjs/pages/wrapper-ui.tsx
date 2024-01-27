302e689f386b7b5eef2d109362e8734dcf009746
Buns Enchantress <68980965+BunsDev@users.noreply.github.com>
2024-01-26 23:08:04 -0600

    add: Bridge to UI (WIP)

--- packages/nextjs/pages/wrapper-ui.tsx
+++ packages/nextjs/pages/wrapper-ui.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import { MetaHeader } from "~~/components/MetaHeader";
+import { BridgeInteraction } from "~~/components/wrapper-ui/BridgeInteraction";
 // import { ContractData } from "~~/components/wrapper-ui/ContractData"
 import { WrapInteraction } from "~~/components/wrapper-ui/WrapInteraction";
 
@@ -12,8 +13,29 @@ const WrapperUI: NextPage = () => {
         <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree&display=swap" rel="stylesheet" />
       </MetaHeader>
       <div className="grid flex-grow" data-theme="exampleUi">
-        <WrapInteraction />
+        <div
+          className="grids text-center text-2xl font-bold justify-center rounded-lg border-8 p-0 m-1.5"
+          style={{
+            borderLeftColor: "#005AFF", // BLUE
+            borderRightColor: "#005AFF", // BLUE
+            fontWeight: "bold",
+          }}
+        >
+          {`Upgrade fMULTI`}
+          <WrapInteraction />
+        </div>
         {/* <ContractData /> */}
+        <div
+          className="grids text-center text-2xl font-bold justify-center rounded-lg border-8 p-0 m-1.5"
+          style={{
+            borderLeftColor: "#005AFF", // BLUE
+            borderRightColor: "#005AFF", // BLUE
+            fontWeight: "bold",
+          }}
+        >
+          {`Bridge lz-fMULTI`}
+          <BridgeInteraction />
+        </div>
       </div>
     </>
   );
