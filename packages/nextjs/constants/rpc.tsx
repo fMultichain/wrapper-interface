3d2700862b81350ad30cd1ca6515d1751cc00540
Buns Enchantress <68980965+BunsDev@users.noreply.github.com>
2024-02-01 01:26:10 -0600

    + constants: RPC

---
+++ packages/nextjs/constants/rpc.tsx
@@ -0,0 +1,15 @@
+import { ChainMap } from "~~/types/ChainMap";
+
+export const ETH_RPC_URL = "https://rpc.ankr.com/eth";
+export const FTM_RPC_URL = "https://ankr.com/fantom";
+export const BASE_RPC_URL = "https://rpc.ankr.com/base";
+export const ARB_RPC_URL = "https://rpc.ankr.com/arbitrum";
+export const AVAX_RPC_URL = "https://rpc.ankr.com/avalanche";
+
+export const RPC: ChainMap = {
+  1: ETH_RPC_URL,
+  250: FTM_RPC_URL,
+  8453: BASE_RPC_URL,
+  42161: ARB_RPC_URL,
+  43114: AVAX_RPC_URL,
+};
